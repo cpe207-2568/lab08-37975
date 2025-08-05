017752f89519b5b92048c64196b832a43fa787ea
TIM <86812783+Theejuth@users.noreply.github.com>
2025-08-06 02:37:51 +0700

    COMPLETE!

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,53 @@
 import Header from "./component/header";
-import  Sidebar from "./component/sidebar";
+import Sidebar from "./component/sidebar";
 import TaskInput from "./component/taskinput.tsx";
-import Task from  "./component/task"
+import Task from "./component/task";
 import type { TaskCardProps } from "./libs/types.ts";
-import Footer  from "./component/footer";
-
+import Footer from "./component/footer";
 
 function App() {
-  const Tasks:TaskCardProps[] = [
-  { id: 1, title: "Read a book", description: "Vite + React + Bootstrap + TS", isDone: false },
-  { id: 2, title: "Write code", description: "Finish project for class", isDone: true },
-  { id: 3, title: "Deploy app", description: "Push project to GitHub Pages", isDone: false },
+  const Tasks: TaskCardProps[] = [
+    {
+      id: 1,
+      title: "Read a book",
+      description: "Vite + React + Bootstrap + TS",
+      isDone: false,
+    },
+    {
+      id: 2,
+      title: "Write code",
+      description: "Finish project for class",
+      isDone: true,
+    },
+    {
+      id: 3,
+      title: "Deploy app",
+      description: "Push project to GitHub Pages",
+      isDone: false,
+    },
   ];
   return (
     <div className="d-flex flex-column" style={{ minHeight: "100vh" }}>
-      
       <Header></Header>
 
       <div className="d-flex flex-grow-1">
         {/* Sidebar ด้านซ้าย */}
-      <Sidebar userName="Teejutha" type="student" />
+        <Sidebar userName="Teejutha" type="student" />
         {/* Main content */}
         <div className="container text-center">
           <main className="flex-grow-1 p-4">
             {/* input ด้านบน*/}
             <TaskInput />
             {/* Card รายการ */}
-            <Task {...Tasks[0]}/>
-            <Task {...Tasks[1]}/>
-            <Task {...Tasks[2]}/>
-            
+            <Task {...Tasks[0]} />
+            <Task {...Tasks[1]} />
+            <Task {...Tasks[2]} />
           </main>
         </div>
       </div>
 
       {/* Footer ด้านล่าง */}
-      <Footer year="2025" fullName="Teejutha Konsombat" studentId={670610708}/>
+      <Footer year="2025" fullName="Teejutha Konsombat" studentId={670610708} />
     </div>
   );
 }
